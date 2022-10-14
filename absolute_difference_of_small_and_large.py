b29fc6165367b3ccba2713e8bbcc52ef58bdf56a
SUTRAYETH HARI YUKTHA NANDA <97426182+21MH1A05C9@users.noreply.github.com>
2022-10-14 23:09:17 +0530

    s=list(map(str,input().split())) a=b=0 for i in s:     s1=ord(min(i))     a+=s1     s2=ord(max(i))     b+=s2     print(abs(a-b),end=' ')     a=b=0

--- absolute_difference_of_small_and_large.py
+++ absolute_difference_of_small_and_large.py
@@ -1,4 +1,9 @@
-n=input()
-n=n.split(" ")
-for i in range(len(n)):
-    print(abs(ord(max(n[i]))-ord(min(n[i]))),end=' ')
+s=list(map(str,input().split()))
+a=b=0
+for i in s:
+    s1=ord(min(i))
+    a+=s1
+    s2=ord(max(i))
+    b+=s2
+    print(abs(a-b),end=' ')
+    a=b=0
